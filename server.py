a1037f2d1ed010ff457f2f732a19d701234965c1
yashhhhh1 <yash8511132161@gmail.com>
2024-11-22 11:49:45 +0530

    add

--- server.py
+++ server.py
@@ -29,4 +29,5 @@ def predict_home_price():
 if __name__ == "__main__":
     from werkzeug.serving import run_simple
     util.load_saved_artifacts()
-    run_simple('0.0.0.0', 5000, app)
+    run_simple('0.0.0.0', 5000, app) 
+    
